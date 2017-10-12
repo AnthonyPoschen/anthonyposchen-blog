948ed377b7279d5845fa6818a7d327cf868dde71
Anthony "Zanven" Poschen <zanven42@gmail.com>
2017-10-12 17:56:57 +1100

    moved file locations

--- protocgen.bash
+++ protocgen.bash
@@ -1,19 +1,33 @@
-services=(
+goservices=(
 	helloworld
 )
-for service in ${services[@]} 
+
+tsFrontendservices=(
+	helloworld
+)
+#admin section as its own SPA seperate from main blog.
+tsAdminServices=(
+	helloworld
+)
+
+for service in ${goservices[@]} 
 do
-	echo generated: ${service}
 	# generate the golang file
 	protoc -I proto/ proto/${service}.proto --go_out=plugins=grpc:proto
+	mv ./proto/${service}.pb.go ./services/${service}/
+done
+
+for service in ${tsFrontendservices[@]}
+do
+
 	# generate the typescript files
 	protoc \
-	--plugin=protoc-gen-ts=./frontend/node_modules/.bin/protoc-gen-ts \
+	--plugin=protoc-gen-ts=./services/frontend/web/node_modules/.bin/protoc-gen-ts \
 	--js_out=import_style=commonjs,binary:proto \
 	--ts_out=service=true:proto \
 	-I ./proto \
 	proto/${service}.proto
 	
-	mv ./proto/${service}.pb.go ./services/${service}/
-	mv ./proto/${service}_pb* ./frontend/src/services/
+	mv ./proto/${service}_pb* ./services/frontend/web/src/services/
+	rm ./proto/${service}_pb*
 done
